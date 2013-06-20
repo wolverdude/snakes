25ea1de97964eaf3a846b7b6bec4aeafbef7e310
App Academy Student <student@appacademy.io>
2013-06-19 23:24:13 -0700

    Add snake movement and turning.

--- snake-app.js
+++ snake-app.js
@@ -1,8 +1,66 @@
 Snake = (function() {
 
-	function Snake() {
-
+	function Game(rows, cols) {
+		this.board = new Board(rows, cols)
+		this.snake = new Snake({row: 6, col: 6}, "north")
 	}
 
-	return Snake
+	Game.prototype.step = function() {
+		this.snake.updateVel()
+		return this.snake.move()
+	}
+
+	function Board(rows, cols) {
+		this.rows = rows
+		this.cols = cols
+	}
+
+	function Snake(pos, dir) {
+		this.body = [pos]
+		this.growths = 2
+		this.dir = dir
+		this.vel = {row: 2, col: 2} //this ensures that any new direction will work
+		this.updateVel()
+	}
+
+	Snake.prototype.turn = function(dir) {
+		this.dir = dir
+	}
+
+	Snake.prototype.updateVel = function() {
+		var newVel = { row: 0, col: 0 }
+
+		if (this.dir == "north") { newVel.row -= 1 }
+		else if (this.dir == "south") { newVel.row += 1 }
+		else if (this.dir == "east") { newVel.col += 1 }
+		else if (this.dir == "west") { newVel.col -= 1 }
+
+		// can only make right turns
+		if ((this.vel.row + newVel.row !== 0) && (this.vel.col + newVel.col !== 0)){
+			this.vel = newVel
+		}
+		// else vel not updated.
+	}
+
+	Snake.prototype.move = function() {
+		var headPos = this.body[this.body.length - 1]
+		var newHeadPos = {
+			row: headPos.row + this.vel.row,
+			col: headPos.col + this.vel.col
+		}
+
+		this.body.push(newHeadPos)
+
+		if (this.growths) {
+			this.growths--
+		} else {
+			var oldTailPos = this.body.shift()
+		}
+
+		return {newHead: newHeadPos, oldTail: oldTailPos }
+	}
+
+	return {
+		Game: Game
+	}
 })();
--- snake-ui.js
+++ snake-ui.js
@@ -1,9 +1,11 @@
 Snake.init = function() {
 
-	var gameSpace = $("#gameSpace");
+	var STEP_TIME_MSEC = 250
+	var $GAME_SPACE = $("#gameSpace");
+	var ROWS = 15
+	var COLS = 25
 
 	function generateBoard(rows, cols) {
-
 		var gameSpaceHeight = $(window).height() - 100;
 		var gameSpaceWidth = $(window).width() - 100;
 
@@ -20,35 +22,59 @@ Snake.init = function() {
 
 		var gameSquareSide = height - 2
 
-		gameSpace.css("height", gameSpaceHeight);
-		gameSpace.css("width", gameSpaceWidth);
+		$GAME_SPACE.css("height", gameSpaceHeight);
+		$GAME_SPACE.css("width", gameSpaceWidth);
 
 		_.times(rows, function(rowNum) {
-			var rowDiv = $("<div>");
-			rowDiv.css("height", height)
+			var $rowDiv = $("<div>");
+			$rowDiv.css("height", height)
 
 			_.times(cols, function(colNum) {
-				var colDiv = $("<div>")
-				colDiv.css("width", gameSquareSide)
-				colDiv.css("height", gameSquareSide)
-				colDiv.attr("class", "gameSquare")
-				colDiv.attr("id", rowNum + "-" + colNum)
+				var $colDiv = $("<div>")
+				$colDiv.css("width", gameSquareSide)
+				$colDiv.css("height", gameSquareSide)
+				$colDiv.attr("class", "gameSquare")
+				$colDiv.attr("id", rowNum + "-" + colNum)
 
-				rowDiv.append(colDiv)
+				$rowDiv.append($colDiv)
 			});
 
-			gameSpace.append(rowDiv)
+			$GAME_SPACE.append($rowDiv)
 		});
 	}
 
+	game = new Snake.Game()
+
 	$("#init").click(function() {
 		$(".message").html("You pressed the button!")
+
+
+		window.setInterval(function() {
+			var diffs = game.step()
+
+			$GAME_SPACE.find(".head").removeClass('head')
+
+			var newHeadId = "#" + diffs.newHead.row + "-" + diffs.newHead.col
+			$GAME_SPACE.find(newHeadId).addClass('snake head')
+
+			if (diffs.oldTail) {
+				var oldTailId = "#" + diffs.oldTail.row + "-" + diffs.oldTail.col
+				$GAME_SPACE.find(oldTailId).removeClass('snake')
+			}
+		}, STEP_TIME_MSEC);
+
+		key('up', function() { game.snake.turn("north") });
+		key('down', function() { game.snake.turn("south") });
+		key('right', function() { game.snake.turn("east") });
+		key('left', function() { game.snake.turn("west") });
+
 	});
 
-	generateBoard(15, 25)
-	var space = gameSpace.find("#5-5")
+	generateBoard(ROWS, COLS)
+	var space = $GAME_SPACE.find("#5-5")
 	console.log(space)
-	space.addClass('snake')
+	space.addClass('apple')
+	$GAME_SPACE.find("#7-7").addClass('wall')
 }
 
 $(Snake.init)
