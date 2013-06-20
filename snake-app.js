e89f0af4ddc194993af41d5bf26522cbc2fc9662
App Academy Student <student@appacademy.io>
2013-06-19 21:39:29 -0700

    Add dynamic game board creation.

--- snake-app.js
+++ snake-app.js
@@ -1,8 +1,8 @@
-(function() {
+Snake = (function() {
 
 	function Snake() {
 
 	}
 
-	return {Snake: Snake}
+	return Snake
 })();
--- snake-ui.js
+++ snake-ui.js
@@ -1,6 +1,54 @@
 Snake.init = function() {
 
-	var WIDTH = navigator.width
-	var HEIGHT = navigator.height
+	var gameSpace = $("#gameSpace");
 
+	function generateBoard(rows, cols) {
+
+		var gameSpaceHeight = $(window).height() - 100;
+		var gameSpaceWidth = $(window).width() - 100;
+
+		var height = Math.floor(gameSpaceHeight / rows);
+		var width = Math.floor(gameSpaceWidth / cols);
+
+		if (height < width) {
+			width = height
+			gameSpaceWidth = width * cols
+		} else {
+			height = width
+			gameSpaceHeight = height * rows
+		}
+
+		var gameSquareSide = height - 2
+
+		gameSpace.css("height", gameSpaceHeight);
+		gameSpace.css("width", gameSpaceWidth);
+
+		_.times(rows, function(rowNum) {
+			var rowDiv = $("<div>");
+			rowDiv.css("height", height)
+
+			_.times(cols, function(colNum) {
+				var colDiv = $("<div>")
+				colDiv.css("width", gameSquareSide)
+				colDiv.css("height", gameSquareSide)
+				colDiv.attr("class", "gameSquare")
+				colDiv.attr("id", rowNum + "-" + colNum)
+
+				rowDiv.append(colDiv)
+			});
+
+			gameSpace.append(rowDiv)
+		});
+	}
+
+	$("#init").click(function() {
+		$(".message").html("You pressed the button!")
+	});
+
+	generateBoard(15, 25)
+	var space = gameSpace.find("#5-5")
+	console.log(space)
+	space.addClass('snake')
 }
+
+$(Snake.init)
